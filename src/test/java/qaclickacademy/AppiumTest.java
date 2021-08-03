50fe918bdb2a6b433df8a738eba4e042e1696637
sukanya <sukanyasreenivas@gmail.com>
2021-08-03 17:00:03 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeAppAndriod() {
+		System.out.println("NativeAppAndriod");
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+		
+	}
+
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from maven
+	//hpw to execute all tcs from test folder - mvn test
+	@Test
+	public void BrowserAutomation() {
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI() {
+		System.out.println("ElementsUI");
+		
+	}
+
+}
